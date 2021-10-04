56415a23b3e2cf7996df461d78e015d83da7b841
deda-official <87776847+deda-official@users.noreply.github.com>
2021-10-04 14:32:56 +0700

    first commit

---
+++ Controllers/HealthzController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DockerAPI.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class HealthzController : ControllerBase
+  {
+    [HttpGet]
+    public ActionResult<string> Get()
+    {
+      return "OK";
+    }
+  }
+}
